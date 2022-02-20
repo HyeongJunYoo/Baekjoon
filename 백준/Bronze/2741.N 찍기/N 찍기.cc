0e6969ec5d29bda9c6636d31e54340de28361e3e
HyeongJunYoo <87575546+HyeongJunYoo@users.noreply.github.com>
2022-02-20 22:12:24 +0900

    [Bronze III] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741.N 찍기/N 찍기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int count = 0;
+
+	cin >> count;
+
+	for (int i = 0; i < count; i++) {
+		printf("%d\n", i+1);
+	}
+
+	return 0;
+
+}
